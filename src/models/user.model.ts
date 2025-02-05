7d4a152db21b031ce4499915b0c36f83c584fbdc
Parth Kikani <parthkikani02@gmail.com>
2025-02-05 13:04:59 +0200

    update user model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,17 +1,23 @@
-import mongoose from 'mongoose';
+import mongoose, { Document, Schema, Model } from 'mongoose';
 import bcrypt from 'bcrypt';
 import config from 'config';
 
-export interface UserDocument extends mongoose.Document {
+// Define an interface for the user fields
+interface IUser {
   email: string;
   name: string;
   password: string;
+}
+
+// Extend IUser with Mongoose Document & Methods
+export interface UserDocument extends IUser, Document {
   createdAt: Date;
   updatedAt: Date;
   comparePassword(candidatePassword: string): Promise<boolean>;
 }
 
-const userSchema = new mongoose.Schema(
+// Define Mongoose schema
+const userSchema = new Schema<UserDocument>(
   {
     email: { type: String, required: true, unique: true },
     name: { type: String, required: true },
@@ -22,25 +28,22 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-userSchema.pre('save', async function (next) {
-  /* 'this' refers to the current user document being saved.
-  /*  We explicitly cast it to UserDocument for type safety in TypeScript. */
+// Pre-save hook to hash password
+userSchema.pre<UserDocument>('save', async function (next) {
+  // 'this' refers to the current user document being saved.
+  // We explicitly cast it to UserDocument for type safety in TypeScript
 
   const user = this as UserDocument;
 
-  if (!user.isModified('password')) {
-    return next();
-  }
+  if (!user.isModified('password')) return next();
 
   const salt = await bcrypt.genSalt(config.get<number>('saltWorkFactor'));
+  user.password = await bcrypt.hash(user.password, salt);
 
-  const hash = await bcrypt.hash(user.password, salt);
-
-  user.password = hash;
-
-  return next();
+  next();
 });
 
+// Define comparePassword method
 userSchema.methods.comparePassword = async function (
   candidatePassword: string
 ): Promise<boolean> {
@@ -49,6 +52,10 @@ userSchema.methods.comparePassword = async function (
   return bcrypt.compare(candidatePassword, user.password).catch((e) => false);
 };
 
-const UserModel = mongoose.model('User', userSchema);
+// Create a Mongoose model
+const UserModel: Model<UserDocument> = mongoose.model<UserDocument>(
+  'User',
+  userSchema
+);
 
 export default UserModel;
