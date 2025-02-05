dbcbac86708edc14217bb69084a7d49ea14f536b
Parth Kikani <parthkikani02@gmail.com>
2025-02-05 15:52:36 +0200

    add session model

---
+++ src/models/session.model.ts
@@ -0,0 +1,32 @@
+import mongoose, { Document, Schema, Model } from 'mongoose';
+import bcrypt from 'bcrypt';
+import config from 'config';
+import { UserDocument } from './user.model';
+
+export interface SchemaDocument extends Document {
+  user: UserDocument['_id'];
+  valid: boolean;
+  userAgent: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+// Define Mongoose schema
+const sessionSchema = new Schema<SchemaDocument>(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    valid: { type: Boolean, default: true },
+    userAgent: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Create a Mongoose model
+const SessionModel: Model<SchemaDocument> = mongoose.model<SchemaDocument>(
+  'Session',
+  sessionSchema
+);
+
+export default SessionModel;
