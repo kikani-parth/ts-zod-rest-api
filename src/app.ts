0fcbfb7b8db587c09e2acb679d39e96b65ab163b
Parth Kikani <parthkikani02@gmail.com>
2025-02-05 11:16:05 +0200

    add db connect function

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import config from 'config';
+import connect from './utils/connect';
 
 const port = config.get<number>('port');
 
 const app = express();
 
-app.listen(port, () => {
+app.listen(port, async () => {
   console.log(`App is running on port ${port}`);
+
+  await connect();
 });
