c47047bde85d540e7161d7feaaf812a481740898
Parth Kikani <parthkikani02@gmail.com>
2025-02-08 11:42:40 +0200

    ass jwt sign and verify functions

---
+++ src/utils/jwt.utils.ts
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+import config from 'config';
+
+const privateKey = config.get<string>('privateKey');
+const publicKey = config.get<string>('publicKey');
+
+export function signJwt(payload: Object, options: jwt.SignOptions = {}) {
+  return jwt.sign(payload, privateKey, { algorithm: 'RS256', ...options });
+}
+
+function verifyJwt(token: string) {
+  try {
+    const decoded = jwt.verify(token, publicKey);
+    return {
+      valid: true,
+      expired: false,
+      decoded,
+    };
+  } catch (e: any) {
+    return {
+      valid: false,
+      expired: e.message === 'jwt expired',
+      decoded: null,
+    };
+  }
+}
