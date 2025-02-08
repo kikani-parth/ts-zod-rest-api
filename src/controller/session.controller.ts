a6a28eedc9ae2e76249bec294a50a4df7468f4ae
Parth Kikani <parthkikani02@gmail.com>
2025-02-08 12:23:23 +0200

    add route to create a session

--- src/controller/session.controller.ts
+++ src/controller/session.controller.ts
@@ -1,20 +1,37 @@
 // session.controller.ts
 
 import { Response, Request } from 'express';
+import config from 'config';
 import { validatePassword } from '../service/user.service';
 import { createSession } from '../service/session.service';
+import { signJwt } from '../utils/jwt.utils';
 
 export async function createUserSessionHandler(req: Request, res: Response) {
   // Validate the user's password
   const user = await validatePassword(req.body);
 
   if (!user) {
-    return res.status(401).send('Invalid email or password');
+    res.status(401).send('Invalid email or password');
+    return;
   }
   // Create a session
   const userAgent = req.get('user-agent') || '';
-  const session = createSession(user._id.toString(), userAgent);
+  const session = await createSession(user._id.toString(), userAgent);
+
   // Create an access token
+  const accessToken = signJwt(
+    { ...user, session: session._id },
+    { expiresIn: config.get('accessTokenTtl') } // 15 mins
+  );
+
   // Create a refresh token
+  const refreshToken = signJwt(
+    { ...user, session: session._id },
+    { expiresIn: config.get('refreshTokenTtl') } // 1 year
+  );
+
   // return access and refresh tokens
+  res.send({ accessToken, refreshToken });
 }
+
+export async function getUserSessionsHandler(req: Request, res: Response) {}
--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,8 @@ import { Express, Request, Response } from 'express';
 import { createUserHandler } from './controller/user.controller';
 import validate from './middleware/validateResource';
 import { createUserSchema } from './schema/user.schema';
+import { createUserSessionHandler } from './controller/session.controller';
+import { createSessionSchema } from './schema/session.schema';
 
 function routes(app: Express) {
   app.get('/healthcheck', (req: Request, res: Response) => {
@@ -11,6 +13,12 @@ function routes(app: Express) {
   });
 
   app.post('/api/users', validate(createUserSchema), createUserHandler);
+
+  app.post(
+    '/api/sessions',
+    validate(createSessionSchema),
+    createUserSessionHandler
+  );
 }
 
 export default routes;
